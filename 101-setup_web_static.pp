755b6d41e1928849424f53c5d58fe82043ceb0bc
Yusuf <52452034+ygbadamosi662@users.noreply.github.com>
2023-04-19 22:06:13 +0100

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -7,16 +7,13 @@ $nginx_conf = "server {
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
-
     location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
+	alias /data/web_static/current;
+	index index.html index.htm;
     }
-
     location /redirect_me {
-        return 301 http://cuberule.com/;
+    	return 301 http://cuberule.com/;
     }
-
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
@@ -46,8 +43,8 @@ file { '/data/web_static/releases/test':
 } ->
 
 file { '/data/web_static/shared':
-  ensure => 'directory'
-} ->
+ensure => 'directory'
+  } ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
