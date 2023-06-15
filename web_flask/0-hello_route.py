a323deef1d4d19dc9eaffd51acb821911ff556d8
Ubuntu <gbadamosiy662@gmail.com>
2023-06-15 09:03:00 +0000

    okay

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,18 +3,16 @@
 
 the application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
+    /airbnb-onepage/: Displays 'Hello HBNB!'
 """
 from flask import Flask
 
+
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
     return "Hello HBNB!"
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
